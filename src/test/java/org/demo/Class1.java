7a267e5959c3e9c746dde068653ff1549f3c3cdb
Udhay2393 <udhay.shankar0323@gmail.com>
2021-11-14 00:06:57 +0530

    Again done

---
+++ src/test/java/org/demo/Class1.java
@@ -0,0 +1,17 @@
+package org.demo;
+
+public class Class1 {
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void meth3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
